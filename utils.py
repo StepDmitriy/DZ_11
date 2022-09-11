f1da1dad0a89c693d74e140fd5577c8509fdde21
StepDmitriy <09ersd14>
2022-09-11 18:18:31 +0400

    Initial commit домашняя работа 11

---
+++ utils.py
@@ -0,0 +1,37 @@
+import json
+
+
+def load_candidates_from_json() -> list[dict]:
+    with open('candidates.json', 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+
+# print(load_candidates_from_json())
+
+def get_candidate(candidate_id: int) -> dict:
+    for candidate in load_candidates_from_json():
+        if candidate["id"] == candidate_id:
+            return candidate
+
+
+# print(get_candidate(1))
+
+
+def get_candidates_by_name(candidate_name: str) -> list[dict]:
+    result = []
+    for candidate in load_candidates_from_json():
+        if candidate["name"] == candidate_name:
+            result.append(candidate)
+    return result
+
+
+# print(get_candidates_by_name("Sheree Love"))
+
+def get_candidates_by_skill(skill_name: str) -> list[dict]:
+    result = []
+    for candidate in load_candidates_from_json():
+        if skill_name in candidate["skills"].lower().split(", "):
+            result.append(candidate)
+    return result
+
+# print(get_candidates_by_skill("python"))
